fe79ca0e29aaa1914517987597a60fe687489d3c
cwechsberg91 <54370058+cwechsberg91@users.noreply.github.com>
2019-11-19 22:46:41 -0500

    Add files via upload

---
+++ Shoe-Decider.py
@@ -0,0 +1,12 @@
+name = input("What is your name? ")
+print ("Hello " + name,". Welcome to the Shoe-Decider! We will help you decide what shoes you should wear! ")
+
+# Question One
+temperature = input("Is it raining today? Yes or No >>") .lower()
+
+if temperature == "no" :
+    print("Nice! Are you feeling comfy or dressy? >>")
+else :
+    print("Oh no.")
+
+
